b50366df1866c0daf4dcfe747f2a9fc178708bf4
yany2 <yuan.yan@dianrong.com>
2018-09-03 09:44:31 +0800

    update config

---
+++ config/webpack.config.base.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /.(js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: [
+          path.resolve(__dirname, '../node_modules')
+        ]
+      },
+      {
+        test: /.js$/,
+        exclude: [
+          path.join(__dirname, '../node_modules')
+        ],
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
---
+++ config/webpack.config.client.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.config.base');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const config = merge(common, {
+  mode: 'development',
+  entry: {
+    app: path.join(__dirname, '../client/index.js')
+  },
+  output: {
+    filename: '[name].[hash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '../client/template.html')
+    }),
+    new HtmlWebpackPlugin({
+      template: '!!ejs-compiled-loader!' + path.join(__dirname, '../client/server.template.ejs'),
+      filename: 'server.ejs'
+    })
+  ],
+});
+
+if (isDev) {
+  config.devServer = {
+    host: '0.0.0.0',
+    port: '8888',
+    contentBase: path.join(__dirname, '../dist'),
+    hot: true,
+    overlay: {
+      errors: true
+    },
+    publicPath: '/public', // 删除dist目录
+    historyApiFallback: {
+      index: '/public/index.html'
+    }
+  };
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+}
+
+module.exports = config;
